8148035a35ad08ed1b47e84e5aebf252bc678fd0
academintellekt-cmd <academintellekt@gmail.com>
2025-12-16 22:47:39 +0700

    Эту версию загрузил на сайт

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,7 @@ tar -czf quiz-deploy.tar.gz \
     --exclude=ИНСТРУКЦИЯ_ЗАПУСКА.md \
     --exclude=УСТАНОВКА.md \
     --exclude=public/fonts \
-    server.js package.json package-lock.json public/ scripts/start.sh nodemon.json server/ data/ tests/
+    server.js package.json package-lock.json public/ scripts/start.sh nodemon.json server/ data/ tests/ docs/
 
 echo "📤 Загрузка файлов на сервер..."
 echo "💡 Введите пароль от сервера:"
